1c25bfc8380713239e42b2515389d8fd8b191f3e
Вячеслав <slavvva05@gmail.com>
2025-10-10 17:25:30 +0300

    Создание моделей базы данных
    Добавленные модели:
      - Student
      - User
      - Role
      - Contact
      - Room
      - Group
      -AppDbContext

---
+++ CmdntBackend/CmdntApi/Models/Room.cs
@@ -0,0 +1,16 @@
+﻿namespace CmdntApi.Models
+{
+    public partial class Room
+    {
+        public int Id { get; set; }
+
+        public int FloorNumber { get; set; }
+
+        public int RoomNumber { get; set; }
+
+        public int Capacity { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; } = new List<Student>();
+    }
+
+}
---
+++ CmdntBackend/CmdntApi/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace CmdntApi.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+
+        public string Surname { get; set; } = null!;
+
+        public string Name { get; set; } = null!;
+
+        public string? Patronymic { get; set; }
+
+        public string Login { get; set; } = null!;
+
+        public string HashPassword { get; set; } = null!;
+
+        public int RoleId { get; set; }
+
+
+        public virtual Role Role { get; set; } = null!;
+    }
+}
