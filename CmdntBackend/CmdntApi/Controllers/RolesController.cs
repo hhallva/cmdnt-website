9ba3da6513ed0802ec3e1d468e45279fc135ba9e
Вячеслав <slavvva05@gmail.com>
2025-10-14 19:06:02 +0300

    Добавлены комментарии к RoleController

--- CmdntBackend/CmdntApi/Controllers/RolesController.cs
+++ CmdntBackend/CmdntApi/Controllers/RolesController.cs
@@ -1,19 +1,26 @@
 ﻿using DataLayer.Data;
 using DataLayer.DTOs;
-using DataLayer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace CmdntApi.Controllers
 {
-   
-    [Route("api/[controller]")]
+
+    [SwaggerTag("Управление ролями пользователей")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class RolesController(AppDbContext context) : ControllerBase
     {
         private readonly AppDbContext _context = context;
 
         [HttpGet]
+        [SwaggerOperation(
+            Summary = "Получить все роли",
+            Description = "Метод возвращает список всех доступных ролей пользователей в системе.")]
+        [SwaggerResponse(StatusCodes.Status200OK, "Список ролей успешно возвращен.", Type = typeof(List<RoleDto>))] // Уточнён тип
+        [SwaggerResponse(StatusCodes.Status404NotFound, "Роли не найдены. Возврат сообщения об ошибке.", Type = typeof(ApiErrorDto))]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, "Внутренняя ошибка сервера.", Type = typeof(ApiErrorDto))]
         public async Task<ActionResult<IEnumerable<RoleDto>>> GetRoles()
         {
             var roles = await _context.Roles
@@ -22,9 +29,9 @@ namespace CmdntApi.Controllers
                     Id = r.Id,
                     Name = r.Name,
                 }).ToListAsync();
-         
-            if(!roles.Any())
-                return NotFound("Роли не найдены!");
+
+            if (roles.Count == 0)
+                return NotFound(new ApiErrorDto("Роли не найдены", StatusCodes.Status404NotFound));
             return Ok(roles);
         }
     }
