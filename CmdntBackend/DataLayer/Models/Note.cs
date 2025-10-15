24b102c86503caf2466486bffe3154bcee6fe713
Вячеслав <slavvva05@gmail.com>
2025-10-15 21:44:09 +0300

    Обновлена база данных
    Теперь при удалении пользователя из системы, его заметки остаются, UserId в заметках становиться Null

--- CmdntBackend/DataLayer/Models/Note.cs
+++ CmdntBackend/DataLayer/Models/Note.cs
@@ -6,7 +6,7 @@
 
         public int StudentId { get; set; }
 
-        public int UserId { get; set; }
+        public int? UserId { get; set; }
 
         public string Text { get; set; } = null!;
 
