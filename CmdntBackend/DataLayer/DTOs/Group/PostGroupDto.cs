89b01cd8b63278baa5c544125a68b484ad261043
Вячеслав <slavvva05@gmail.com>
2025-10-16 19:55:01 +0300

    Добавлен контроллер группы
    Возможный функционал:
    + Добавление новой группы
    + Удаление пустых групп
    + Получение списка всех групп
    + Получение группы по id

---
+++ CmdntBackend/DataLayer/DTOs/Group/PostGroupDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer.DTOs.Group
+{
+    public class PostGroupDto
+    {
+        [Required(ErrorMessage = "Название группы обязательно.")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Название группы должно содержать от 1 до 100 символов.")]
+        public string Name { get; set; } = null!;
+
+        [Range(1, 5, ErrorMessage = "Курс обучения может быть в диапазоне от 1 до 5.")]
+        public int Course { get; set; }
+    }
+}
