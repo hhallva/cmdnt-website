9a59bcdb64f34fdb390f24d0e5b880d74b381b76
Вячеслав <slavvva05@gmail.com>
2025-10-19 19:27:58 +0300

    Добавил автовставку для устройств

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -56,6 +56,7 @@ const LoginPage: React.FC = () => {
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
                   required
+                  autoComplete="username"
                 />
               </div>
               {usernameError && (
@@ -77,6 +78,7 @@ const LoginPage: React.FC = () => {
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   required
+                  autoComplete="current-password"
                 />
               </div>
               {passwordError && (
