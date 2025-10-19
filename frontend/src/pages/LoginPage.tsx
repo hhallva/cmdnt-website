02e27b2146777349a91491e30604a06bb20abc39
Вячеслав <slavvva05@gmail.com>
2025-10-19 18:58:31 +0300

    Настроена адаптивность для экрана Авторизации

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import '../index.css';
+
 
 const LoginPage: React.FC = () => {
   const [username, setUsername] = useState('');
