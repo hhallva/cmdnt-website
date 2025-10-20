ef1430d33a50f294e721627afbd88475bd9aa285
Вячеслав <slavvva05@gmail.com>
2025-10-20 16:58:01 +0300

    Обновлен метод авторизации API

--- CmdntBackend/DataLayer/DTOs/Account/LoginDto.cs
+++ CmdntBackend/DataLayer/DTOs/Account/LoginDto.cs
@@ -4,12 +4,8 @@ namespace DataLayer.DTOs.Account
 {
     public class LoginDto
     {
-        [Required(ErrorMessage = "Почта обязательна для заполнения.")]
-        [StringLength(100, ErrorMessage = "Логин не должен превышать 100 символов.")]
         public string Login { get; set; } = null!;
 
-        [Required(ErrorMessage = "Пароль обязателен для заполнения.")]
-        [StringLength(100, MinimumLength = 5, ErrorMessage = "Пароль должен быть от 5 до 100 символов.")]
         public string Password { get; set; } = null!;
     }
 }
