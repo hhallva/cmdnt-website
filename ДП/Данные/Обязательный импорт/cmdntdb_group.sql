9df256ef6b042fcc0da09af0f6a1792729850624
Вячеслав <slavvva05@gmail.com>
2026-05-18 22:45:18 +0300

    + Добавлены основные файлы ДП

---
+++ ДП/Данные/Обязательный импорт/cmdntdb_group.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: cmdntdb
+-- ------------------------------------------------------
+-- Server version	8.0.43
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `group`
+--
+
+LOCK TABLES `group` WRITE;
+/*!40000 ALTER TABLE `group` DISABLE KEYS */;
+INSERT INTO `group` VALUES (1,'ИСПВ-21',4),(2,'ИСПВ-22',4),(3,'ИСПВ-32',3),(4,'ИСПВ-42',2),(5,'ИСПВ-52',1),(6,'ИСПП-21',4),(7,'ИСПП-31',3),(8,'ИСПП-34',3),(9,'ИСПП-35',3),(10,'ИСПП-41',2),(11,'ИСПП-43',2),(12,'ИСПП-45',2),(13,'ИСПП-51',1),(14,'ИСПП-55',1),(15,'ИСС-11',5),(16,'ИСС-12',5),(17,'ИСС-21',4),(18,'ИСС-22',4),(19,'ИСС-25',4),(20,'ИСС-31',3),(21,'ИСС-32',3),(22,'ИСС-35',3),(23,'ИСС-41',2),(24,'ИСС-45',2),(25,'ИСС-51',1),(26,'ИСС-52',1),(27,'КСК-21',4),(28,'КСК-22',4),(29,'КСК-31',3),(30,'КСК-41',2),(31,'КСК-51',1),(32,'ОИБ-21',4),(33,'ОИБ-31',3),(34,'ОИБ-35',3),(35,'ОИБ-41',2),(36,'ОИБ-51',1),(37,'П-41',2),(38,'Р-21',4),(39,'РМТ-31',3),(40,'РМТ-35',3),(41,'РМТ-41',2),(42,'РМТ-45',2),(43,'РМТ-51',1),(44,'РМТ-52',1),(45,'ССА-21',4),(46,'ССА-22',4),(47,'ССА-31',3),(48,'ССА-35',3),(49,'ССА-41',2),(50,'ССА-51',1),(51,'ССА-55',1);
+/*!40000 ALTER TABLE `group` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-05-04 21:24:48
